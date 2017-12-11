63b4a7edce77e57a99431d27c995c56c071c3a01
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-12-11 21:33:33 +0200

    Initial commit + SimpleSingleton + test

---
+++ CodingPlayground/DesignPatterns/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceOne = SimpleSingleton.InstanceProperty;
+            instanceOne.TestProperty = 5;
+            System.Console.WriteLine(instanceOne.TestProperty);
+
+            var instanceTwo = SimpleSingleton.InstanceProperty;
+            System.Console.WriteLine(instanceTwo.TestProperty);
+        }
+    }
+}
---
+++ CodingPlayground/DesignPatterns/SimpleSingleton.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns
+{
+    public class SimpleSingleton
+    {
+        private static SimpleSingleton instanceField;
+
+        public int testProperty;
+
+        private SimpleSingleton()
+        {
+            this.TestProperty = testProperty;
+        }
+
+        public static SimpleSingleton InstanceProperty
+        {
+            get
+            {
+                if (instanceField == null)
+                {
+                    instanceField = new SimpleSingleton();
+                }
+
+                return instanceField;
+            }
+        }
+
+        public int TestProperty { get; set; }
+    }
+}
